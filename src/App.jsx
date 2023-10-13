8491d4761e3ddcb0f8cb1945f5d87fc32e68f4e5
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-14 01:20:48 +0530

    comment line added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-function Hello(){
+function Hello(props){
+  console.log(props)
   return (
-    <p>vanakam bhaa</p>
+    <p>vanakam bhaa {props.username}</p>
   )
 }
 
 function reuseComponent(){
-  return (
+  let user1="deva"
+  let user2="viswa"
+  return (    
     <div>
       <Hello></Hello>
       <Hello />
-
-      
+      <Hello username={user1} />
+      <Hello username={user2} />      
     </div>
   )
 }
+//gonna study
 export default reuseComponent
