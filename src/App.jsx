f2853910b2ebb9a0c045adbc695012725b90edb8
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-14 01:39:46 +0530

    creating component without arrow fn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
-const greeet=() => {
-  return(
-    <div>Hello world!</div>
+//creating a component arrow fn
+
+// const greet= () => {
+//   console.log('hello to console')
+//   return (    
+//     <div>
+//       <p>Hello Deva</p>
+//       <h1>Whats good my man!</h1>
+//     </div>
+//   )
+// }
+// export default greet
+
+//another way of creating component
+function greet(){
+  console.log('hello to console')
+  return (
+    <div>
+      <h1><H></H>ello all</h1>
+      <p>This a para tag , this para is not about lorem ipsum !</p>
+    </div>
   )
 }
-export default greeet
+
+export default greet
