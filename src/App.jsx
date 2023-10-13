2dfd574e8b2d887342289e1f7503d61ddf9a5da1
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-14 02:05:37 +0530

    new dynamic addition prog

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 // dynamically creating html
 
-function timer () {
-  const now = new Date()
+// function timer () {
+//   const now = new Date()
 
-  console.log(now)
+//   console.log(now)
+//   return(
+//     <div>
+//       hello its now {now.toString()}
+//     </div>
+//   )
+// }
+// export default timer
+
+function add () {
+  let a= 20
+  let b=30
   return(
     <div>
-      hello its now {now.toString()}
+      the sum of {a} and {b} is equal {a+b}
     </div>
   )
 }
-export default timer
+export default add
