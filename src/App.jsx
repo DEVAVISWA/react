2451f1c2c1ae98b3fdecaf9828ac4ab889f05ac2
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-15 00:40:03 +0530

    passing props to a container

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
-//reusing component
-function Hello(){
-  return(
-    <p>Hello world</p>
+//Using props
+function Hello(props) {
+  console.log(props)
+  return (
+    <p>Hello {props.userName}</p>
   )
 }
 
-function greeting () {
-  return(
+function greeting() {
+  let user1='deva'
+  let user2='viswa'
+  return (
     <div>
-      Greetings 
-      <Hello />
-      <Hello />
-      <Hello />
-      <Hello/>
-    </div>
+      Greetings
+      <Hello userName={user1} />
+      <Hello userName={user2}/>
+      <Hello userName={'ganesan'} />
+      <Hello userName={'booma'} />
+    </div> 
   )
 }
 
 export default greeting
+
+//eg 2
+// function Message(props) {
+//   console.log(props)
+//   return(
+//     <p>rathiri pathu mani<h1>{props.time}</h1>ku maadi ku vandha</p>
+//   )
+// }
+
+// function greet(){
+//   return (
+//     <div>
+//       <Message time={'rendu mani'}/>
+//     </div>
+//   )
+// }
+// export default greet
