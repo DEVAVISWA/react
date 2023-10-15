bdf234fc3e8989aea6d2ca239413accf5c939043
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-15 23:47:45 +0530

    conditional rendering way 1

--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 function Note({ note }) {
+    if(note.important==true){
+        return (
+            <li > {note.content} ⭐ </li>
+          )
+    }
   return (
     <li > {note.content}  </li>
   )
