5a7003e8c4c4d3e193bdd184f3ecac31eaf404d5
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-16 00:00:57 +0530

    second way conditional rendering using and logical operator

--- src/components/note.jsx
+++ src/components/note.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Note({ note }) {
   return (
-    <li > {note.important==true ? note.content + "⭐" : note.content}  </li>
+    <li > {note.content} {note.important==true && '⭐'} </li>
   )
 }
 
