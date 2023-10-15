2766453d324e63dd66f1ea6111f5c0403efba680
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-15 17:03:25 +0530

    diff ways of passing props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-// function Hello (props){
-//   return (
-//     <p>Hellon {props.username} </p>
-//   )
-// }
+function Hello (props){
+  return (
+    <p>Hellon {props.username} {props.age} </p>
+  )
+}
 
-// function greet (){
-//   let user1="devaviswa"
-//   return (
-//     <div>
-//     <Hello username= {user1} />
-//     </div>
-//   )
-// }
-// export default greet
+function greet (){
+  const AGE= 70
+  return (
+    <div>
+    <Hello username= {"devaviswa"} age= {26-1} />
+    <Hello username={'ganesan'} age= {AGE} />
+    </div>
+  )
+}
+export default greet
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 
-function App() {
-    return (
-        <p>
-            hello from main.jsx
-        </p>
-    )
-}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
