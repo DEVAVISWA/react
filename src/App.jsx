fa5decab84bf0d9fe89020b88468a1541d2f1d38
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-15 16:47:58 +0530

    using main jsx alone to get output

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-function Hello(){
-  return (
-    <p>vanakam bhaa</p>
-  )
-}
+// function Hello (props){
+//   return (
+//     <p>Hellon {props.username} </p>
+//   )
+// }
 
-function reuseComponent(){
-  return (
-    <div>
-      <Hello></Hello>
-      <Hello />
-
-      
-    </div>
-  )
-}
-export default reuseComponent
+// function greet (){
+//   let user1="devaviswa"
+//   return (
+//     <div>
+//     <Hello username= {user1} />
+//     </div>
+//   )
+// }
+// export default greet
