52979b53257f582292002b2020fc01dadcb0cd16
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-15 18:09:02 +0530

    first way of handling array list

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ function App({note}) { //destructuring of obj from main.jsx
   console.log(note)
   return (
     <div>
-      app
+      <ul>
+        <li> {note[0].content} </li>
+        <li> {note[1].content} </li>
+        <li> {note[2].content} </li>
+        <li> {note[3].content} </li>        
+      </ul>
     </div>    
   )
 }
