a1b542bb18d021332a4b4f3d5bb1260b23d2ebfc
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-16 20:09:23 +0530

    mini task

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
-import React from 'react'
-import Note from './components/note'
+// State: state refers to the data
+// that is used to manage and represent the internal state of the component.
+// it allows the components to keep track of and manage their own data -> the data will change over the time as a result of user interactions, data fetching, or other factors.
 
-function App({notes}) { 
- 
-  return (
-    <div>
-      <ul>
-        {
-          notes.map(note=>
-            <Note key={note.id} note={ note }/> )
-        }       
-      </ul>
-      <form >
-        <input type="text" />
-        <button onClick={()=>{
-          console.log('save button clicked')
-        }} >Save Note</button>
-      </form>
-    </div>    
-  )
+// whenever the state of a component changes, it will cause re-rendering of the component
+
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props); // to call the parent class constructor (x*x*x*)
+
+    // define the state
+    this.state = {
+      count: 0,
+    }
+  }
+
+  increment = () => {
+    // update the state (data) of the count
+    this.setState({
+      // provide the new state
+      count: this.state.count + 1,
+    })
+  }
+  decrement= () =>{
+    this.setState({
+      count: this.state.count-1
+    })
+  }
+  reset= () =>{
+    this.setState({
+      count: 0
+    })
+  }
+  render() {
+    return (
+      <div>
+        <p>Count: { this.state.count }</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <button onClick={this.reset}>Reset</button>
+      </div>
+    )
+  }
 }
 
-export default App
+export default App;
--- src/components/note.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Note({ note }) {
-  return (
-    <li > {note.content} {note.important==true && '⭐'} </li>
-  )
-}
-
-export default Note
