6e50c4a7e6d89b697bb725c930a0c6f25db90b01
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-18 21:41:50 +0530

    react router task and day26 task

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import React, {useState} from 'react'
-import Counter from './component/counter'
-import Button from './component/buttons'
-
-
-
+import React from 'react'
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Home from './component/home'
+import Notes from './component/notes'
+import Users from './component/users'
 
 function App() {
-  const [counter, setCounter] = useState(0)
-  function handleClickPlus(){
-    setCounter(counter +1 )
+  const padding ={
+    padding: 5,
+    
   }
-  function handleClickZero(){
-    setCounter(0)
-  }
-  function handleClickMinus(){
-    setCounter(counter-1)
-  }
+
   return (
-    <div>
-      <Counter counter= {counter}/>
-      <Button handleClick={handleClickPlus} name='plus'/>
-      <Button handleClick={handleClickZero} name='zero'/>
-      <Button handleClick={handleClickMinus} name='minus'/>
-       
-      {/* <button onClick={handleClickZero}>Reset</button>
-      <button onClick={handleClickMinus}>Minus</button> */}
-    </div>
+    <Router>
+      <div>
+        <Link to='/' style={padding}>home</Link>
+        <Link to='/notes' style={padding}>notes</Link>
+        <Link to='/users' style={padding}>users</Link>        
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/notes' element= {<Notes />} />
+        <Route path='/users' element= {<Users />} />
+      </Routes>
+    </Router>
   )
 }
 
