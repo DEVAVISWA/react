406ba461a32eb2d66bcd6579a43aa7a2d70c8edc
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-19 19:20:25 +0530

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
 import React from 'react'
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Home from './component/home'
-import Notes from './component/notes'
-import Users from './component/users'
 
-function App() {
-  const padding ={
-    padding: 5,
-    
-  }
-
+export default function App() {
   return (
-    <Router>
-      <div>
-        <Link to='/' style={padding}>home</Link>
-        <Link to='/notes' style={padding}>notes</Link>
-        <Link to='/users' style={padding}>users</Link>        
-      </div>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/notes' element= {<Notes />} />
-        <Route path='/users' element= {<Users />} />
-      </Routes>
-    </Router>
+    <div>App</div>
   )
 }
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,27 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-const notes =[
-    {
-        id: 1,
-        content: 'spot light',
-        important: true
-    },
-    {
-        id: 2,
-        content: "moon light",
-        important: true
-    },
-    {
-        id: 3,
-        content: "what",
-        important: false
-    },
-    {
-        id: 4,
-        content: "didnt you get your moonlight",
-        important: true
-    }
-]
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
