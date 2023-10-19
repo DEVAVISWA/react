1f4ce06033c5e9a1ca4a0acc2d357cbc84b7b172
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-20 00:00:58 +0530

    day 23 task button flipper in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      count:0,
+    constructor (props){
+        super(props)
+        this.state = {
+            isInCart: true
+        }
     }
-  }
- increment = () =>{
-  this.setState({
-    count: this.state.count + 1
-  })
- }
- reset = () =>{
-  this.setState({
-    count: 0
-  })
- }
- decrement = () =>{
-  this.setState({
-    count: this.state.count - 1
-  })
- }
+
+    handleAddToCartButton = () =>{
+        this.setState ({
+            isInCart: ! this.state.isInCart
+        })
+    }
   render() {
     return (
       <div>
-        <p> Count : {this.state.count} </p>
-        <button onClick={this.increment}>Increment</button>
-        <button onClick={this.reset}>Reset</button>
-        <button onClick={this.decrement}>Decrement</button>
+        {
+            this.state.isInCart ?( <button onClick={this.handleAddToCartButton}>Add To Cart</button> ) :
+            ( <button onClick={this.handleAddToCartButton}>Remove from cart</button>)
+        }
       </div>
     )
   }
