ec3057868d6843dbb9d38188d6dad1580e1686f2
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-19 16:35:44 +0530

    changing default state to three states

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,23 @@ export class App extends Component {
     count: this.state.count + 1
   })
  }
+ reset = () =>{
+  this.setState({
+    count: 0
+  })
+ }
+ decrement = () =>{
+  this.setState({
+    count: this.state.count - 1
+  })
+ }
   render() {
     return (
       <div>
         <p> Count : {this.state.count} </p>
         <button onClick={this.increment}>Increment</button>
+        <button onClick={this.reset}>Reset</button>
+        <button onClick={this.decrement}>Decrement</button>
       </div>
     )
   }
