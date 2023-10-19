c665f5b8dfb7180000619907ce554cbc0848d84d
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-19 13:39:48 +0530

    one class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,13 @@
-// State: state refers to the data
-// that is used to manage and represent the internal state of the component.
-// it allows the components to keep track of and manage their own data -> the data will change over the time as a result of user interactions, data fetching, or other factors.
+import React, { Component } from 'react'
 
-// whenever the state of a component changes, it will cause re-rendering of the component
-
-import React, { Component } from 'react';
-
-class App extends Component {
-
-  constructor(props) {
-    super(props); // to call the parent class constructor (x*x*x*)
-
-    // define the state
-    this.state = {
-      count: 0,
-    }
-  }
-
-  increment = () => {
-    // update the state (data) of the count
-    this.setState({
-      // provide the new state
-      count: this.state.count + 1,
-    })
-  }
-  decrement= () =>{
-    this.setState({
-      count: this.state.count-1
-    })
-  }
-  reset= () =>{
-    this.setState({
-      count: 0
-    })
-  }
+export class App extends Component {
   render() {
     return (
       <div>
-        <p>Count: { this.state.count }</p>
-        <button onClick={this.increment}>Increment</button>
-        <button onClick={this.decrement}>Decrement</button>
-        <button onClick={this.reset}>Reset</button>
+        <p>hello world</p>
       </div>
     )
   }
 }
 
-export default App;
+export default App
