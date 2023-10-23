e008fc5605d38d2b0fe2ca35340c01a2143ee57f
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-23 16:55:12 +0530

    CONTEXT API props passing CONSUMER mtd

--- src/components/Childcomp.jsx
+++ src/components/Childcomp.jsx
@@ -4,17 +4,23 @@ import { useContext } from 'react'
 import { MessageContext } from '../App'
 
 function Childcomp() {
-    const {data,coins} = useContext(MessageContext)
+    const { data, coins } = useContext(MessageContext)
     // console.log(data,coins)
-  return (    
-    <div>
-        <h2> Childcomp </h2>
-        <i>message from parent :- {data}  </i> <br />
-        <b> {coins} </b>
-        <hr />
-        <GrandChildComp />
-    </div>
-  )
+    return (
+        <MessageContext.Consumer>
+            {
+                (DATA) => (
+                    <div>
+                        <h2> Childcomp </h2>
+                        <i>message from parent :- {data}  </i> <br />
+                        <b> {coins} </b>
+                        <hr />
+                        <GrandChildComp />
+                    </div>
+                )
+            }
+        </MessageContext.Consumer>
+    )
 }
 
 export default Childcomp
