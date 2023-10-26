dc4ee386797fda2e9deff2c3debdcee4a193a595
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-26 16:57:21 +0530

    selecting multiple tags using qureySelectorAll in useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,73 @@
-import React, { useRef } from 'react'
+import React, { useRef } from 'react';
 
 function App() {
 
-  const bodyRef = useRef(document.body)
+  const rows = useRef();
 
-  const buttonHandler = () => {
-    // console.log(buttonRef.current)
-    // buttonRef.current.style.backgroundColor = 'pink'
-    const colors= ['red','green','pink','blue','yellow','gold', 'silver','black','purple','orange']
-    const randomColor= Math.floor(Math.random()*10)
-    // console.log(randomColor)
-    bodyRef.current.style.backgroundColor= colors[randomColor]
+  const changeRowColor = () => {
+    // select the elements
+    // console.log(rows.current.querySelectorAll('tbody tr'));
+    const rowsRef = rows.current.querySelectorAll('tbody tr');
+    rowsRef.forEach((row, index) => {
+      if (index % 2 == 0) {
+        row.style.backgroundColor = 'lightgrey';
+      }
+    })
   }
-
 
   return (
     <div>
-      <h3>Body color changer</h3>
-      <button onClick={buttonHandler} >Click to change color</button>
+      <table ref={rows}>
+        <thead>
+          <tr>
+            <th>SNO</th>
+            <th>NAME</th>
+            <th>AGE</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Deva</td>
+            <td>25</td>
+          </tr>
+
+          <tr>
+            <td>2</td>
+            <td>mbneqw</td>
+            <td>89</td>
+          </tr>
+
+          <tr>
+            <td>3</td>
+            <td>asdgdf</td>
+            <td>23</td>
+          </tr>
+
+          <tr>
+            <td>4</td>
+            <td>hgjhjkrty</td>
+            <td>768</td>
+          </tr>
+
+          <tr>
+            <td>5</td>
+            <td>uiytvnb</td>
+            <td>354</td>
+          </tr>
+
+          <tr>
+            <td>6</td>
+            <td>ghfvxc</td>
+            <td>645</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <button onClick={changeRowColor}>Change Row Color</button>
     </div>
   )
 }
 
-export default App
+export default App;
