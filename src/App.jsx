d0d2f63fcc4b1a11e6d311b723167620adaafcb6
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-26 23:06:53 +0530

    mock api (avoidthis)

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import React, { useReducer, useState } from 'react';
-import { initialState, reducer } from './reducers/countReducer';
+import React from 'react'
 
 function App() {
-
-  const [state, dispatch] = useReducer(reducer, initialState);
-
   return (
-    <div>
-      <h3>Counter: { state.count }</h3>
-      <button onClick={() => dispatch({ type: 'incr' })}>Increment</button>
-      <button onClick={() => dispatch({ type: 'decr' })}>Decrement</button>
-      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
-
-      <h4>Clicks History</h4>
-      <p>{ state.clickHistory }</p>
-    </div>
+    <div>App</div>
   )
 }
 
-export default App;
+export default App
