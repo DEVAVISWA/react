48e17c7c15b88851893f7d4b2e6dce995305848a
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-26 16:19:43 +0530

    button color changer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useRef } from 'react'
 
 function App() {
 
-  const inputRef = useRef(null)
+  const buttonRef = useRef(null)
 
-  useEffect(()=>{
-    inputRef.current.focus()
-  },[])
+  const buttonHandler = () => {
+    // console.log(buttonRef.current)
+    // buttonRef.current.style.backgroundColor = 'pink'
+    const colors= ['red','green','pink','blue','yellow','gold', 'silver','black','purple','orange']
+    const randomColor= Math.floor(Math.random()*10)
+    // console.log(randomColor)
+    buttonRef.current.style.backgroundColor= colors[randomColor]
+  }
+
 
   return (
     <div>
-      <input type="text" ref={inputRef} />
-      
+      <h3>Button color changer</h3>
+      <button onClick={buttonHandler} ref={buttonRef}>Click to change color</button>
     </div>
   )
 }
 
-export default App 
+export default App
