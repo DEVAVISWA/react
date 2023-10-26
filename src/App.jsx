5db695614b8a8243da68015799cb3bedca0ba258
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-26 15:39:32 +0530

    input focus on page loading and reloading using useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 function App() {
 
   const inputRef = useRef(null)
 
-  const buttonClickHandler = () => {
-    // console.log(inputRef)
-    // console.log(inputRef.current)
-    // console.log(inputRef.current.value)
-    // inputRef.current.value= 'devaviswa'
+  useEffect(()=>{
     inputRef.current.focus()
-  }
+  },[])
 
   return (
     <div>
       <input type="text" ref={inputRef} />
-      <button onClick={buttonClickHandler}>Focus Input</button>
+      
     </div>
   )
 }
