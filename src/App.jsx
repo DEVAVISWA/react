a5a6f6c2c3f87340ac3ff23a5be63e7bc0451602
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-27 18:20:02 +0530

    Create operation 2nd half

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import React, { useRef, useState } from 'react'
 
 function App(props) {
 
-  const newNoteContentRef = useRef(null)
-  const [newNoteContent,setNewNoteContent] = useState('')
-  const[newNoteImportant,setNewNoteImportant]= useState('')
 
   const [notes, setNotes] = useState(props.notes)
+
+  const[showStatus,setShowSatus] = useState('all')
+
+
+  
+  const [newNoteContent,setNewNoteContent] = useState('')
+  const [newNoteImportant,setNewNoteImportant]= useState('true')
+
+  const newNoteContentRef = useRef(null)
 
   
 
@@ -19,14 +25,51 @@ function App(props) {
       import : newNoteImportant === 'true'
     }
     setNotes(notes.concat(noteObject))
+    setNewNoteContent('')
+    setNewNoteImportant('')
+    newNoteContentRef.current.focus()
+  }
+ 
+  
+  const handleStatusChange = (event) => {
+    setShowSatus(event.target.value)
   }
 
+
+
+  const filteredNotes = (notes,showStatus) => {       
+    switch(showStatus) {
+      case 'all' :
+        return notes ;
+      case 'imp' :
+        return notes.filter(note => note.important === true)
+      case 'nonimp' :
+        return notes.filter(note => note.important === false)
+    }
+  }
+  const notesFiltered = filteredNotes(notes,showStatus)
+
+  
   return (
     <div>
       <h1>The Notes are</h1>
+
+      <label>
+        <input type='radio' name='filter' value='all'
+        onChange={handleStatusChange} checked={showStatus ==='all'}/> All Notes
+      </label>
+      <label>
+        <input type='radio' name='filter' value='imp'
+        onChange={handleStatusChange} />Important notes
+      </label>
+      <label>
+        <input type='radio' name='filter' value='nonimp'
+        onChange={handleStatusChange} />Non Important notes
+      </label>
+      
       <ul>
         {
-          notes.map(note =>
+          notesFiltered.map(note =>
             <li key={note.id}>{note.content}</li>
           )
         }
@@ -46,9 +89,9 @@ function App(props) {
           <select 
           onChange={e=> setNewNoteImportant(e.target.value)}
            value={newNoteImportant} >
-            <option disabled>Select option</option>
-            <option value="">true</option>
-            <option value="">false</option>
+            <option disabled>Selectoption</option>
+            <option value='true'>true</option>
+            <option value='false'>false</option>
           </select>
         </label>
         <br />
