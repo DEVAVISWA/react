5106f6ad5bf6fe936f1f1fa78ecb1319d0d91c77
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-27 16:33:02 +0530

    added a controlled component form

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 function App(props) {
 
-  const [notes,setNotes] = useState(props.notes)
+  const newNoteContentRef = useRef(null)
+  const [newNoteContent,setNewNoteContent] = useState('')
+
+  const [notes, setNotes] = useState(props.notes)
   return (
     <div>
-      <h1>Notes are</h1>
+      <h1>The Notes are</h1>
       <ul>
         {
           notes.map(note =>
-            <li key={note.id}>{note.content}</li> 
-            )
+            <li key={note.id}>{note.content}</li>
+          )
         }
       </ul>
+      <hr />
+      <form >
+        <label>
+          content : &nbsp;&nbsp;&nbsp;&nbsp;
+          <input 
+          type="text" ref={newNoteContentRef} value={newNoteContent} 
+          onChange={e => setNewNoteContent(e.target.value)} />
+        </label>
+      </form>
     </div>
   )
 }
