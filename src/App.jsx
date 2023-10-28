4f5be9aae4eb6800db9bf0e019f5fdfe0abecb44
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-28 15:07:33 +0530

    Create option done

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,92 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from 'react';
+
+// 3. read the notes list and render it here
 
 function App(props) {
 
+  // define a state to store the notes from props
+  const [notes, setNotes] = useState(props.notes);
 
-  const [notes, setNotes] = useState(props.notes)
+  const [showStatus, setShowStatus] = useState('all');
 
-  const[showStatus,setShowSatus] = useState('all')
+  // states for adding new note form
+  const [newNoteContent, setNewNoteContent] = useState('');
+  const [newNoteImportant, setNewNoteImportant] = useState('true');
 
-
-  
-  const [newNoteContent,setNewNoteContent] = useState('')
-  const [newNoteImportant,setNewNoteImportant]= useState('true')
-
-  const newNoteContentRef = useRef(null)
-
-  
+  // define a contentRef to access and manipulate the content element
+  const newNoteContentRef = useRef(null);
 
   const addNote = (event) => {
-    event.preventDefault()
-    // console.log('button is clicked')
+    event.preventDefault();
+
+    // create a new note object
     let noteObject = {
-      id: notes.length + 1 ,
-      content : newNoteContent,
-      import : newNoteImportant === 'true'
+      id: notes.length + 1,
+      content: newNoteContent,
+      important: newNoteImportant == 'true',
     }
-    setNotes(notes.concat(noteObject))
-    setNewNoteContent('')
-    setNewNoteImportant('')
-    newNoteContentRef.current.focus()
+
+    setNotes(notes.concat(noteObject));
+
+    // clear the inputs
+    setNewNoteContent('');
+    setNewNoteImportant('');
+
+    newNoteContentRef.current.focus();
   }
- 
-  
+
   const handleStatusChange = (event) => {
-    setShowSatus(event.target.value)
+    setShowStatus(event.target.value);
   }
 
-
-
-  const filteredNotes = (notes,showStatus) => {       
-    switch(showStatus) {
-      case 'all' :
-        return notes ;
-      case 'imp' :
-        return notes.filter(note => note.important === true)
-      case 'nonimp' :
-        return notes.filter(note => note.important === false)
+  const filterNotes = (notes, showStatus) => {
+    switch (showStatus) {
+      case 'all':
+        return notes;
+      case 'imp':
+        return notes.filter(note => note.important === true);
+      case 'nonimp':
+        return notes.filter(note => note.important === false);
     }
   }
-  const notesFiltered = filteredNotes(notes,showStatus)
 
-  
+  const notesFiltered = filterNotes(notes, showStatus);
+
   return (
     <div>
-      <h1>The Notes are</h1>
+      <h1>Notes</h1>
 
       <label>
-        <input type='radio' name='filter' value='all'
-        onChange={handleStatusChange} checked={showStatus ==='all'}/> All Notes
+        <input
+          type='radio'
+          name='filter'
+          value='all'
+          onChange={handleStatusChange}
+          checked={showStatus === 'all'}
+        />
+        All Notes
       </label>
+
       <label>
-        <input type='radio' name='filter' value='imp'
-        onChange={handleStatusChange} />Important notes
+        <input
+          type='radio'
+          name='filter'
+          value='imp'
+          onChange={handleStatusChange}
+        />
+        Important Notes
       </label>
+
       <label>
-        <input type='radio' name='filter' value='nonimp'
-        onChange={handleStatusChange} />Non Important notes
+        <input
+          type='radio'
+          name='filter'
+          value='nonimp'
+          onChange={handleStatusChange}
+        />
+        Non-Important Notes
       </label>
-      
+
       <ul>
         {
           notesFiltered.map(note =>
@@ -74,32 +94,35 @@ function App(props) {
           )
         }
       </ul>
-      <hr />
+      <hr></hr>
+      <h2>Add a New Note</h2>
       <form onSubmit={addNote}>
         <label>
-          Content : &nbsp;&nbsp;&nbsp;&nbsp;
-          <input 
-          type="text" ref={newNoteContentRef} value={newNoteContent} 
-          onChange={e => setNewNoteContent(e.target.value)} />
+          Content: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input
+            type='text'
+            ref={newNoteContentRef}
+            value={newNoteContent}
+            onChange={e => setNewNoteContent(e.target.value)}
+          />
         </label>
-        <br />
-        <br />
+        <br /><br />
         <label>
-          Important : &nbsp;
-          <select 
-          onChange={e=> setNewNoteImportant(e.target.value)}
-           value={newNoteImportant} >
-            <option disabled>Selectoption</option>
+          Is important: &nbsp;&nbsp;
+          <select
+            onChange={e => setNewNoteImportant(e.target.value)}
+            value={newNoteImportant}
+          >
+            <option disabled>--select--</option>
             <option value='true'>true</option>
             <option value='false'>false</option>
           </select>
         </label>
-        <br />
-        <br />  
-        <button type='submit'>Add the note</button>
+        <br /><br />
+        <button type='submit'>Add New Note</button>
       </form>
     </div>
   )
 }
 
-export default App
+export default App;
