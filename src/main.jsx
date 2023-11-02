35341f60d9b5aa4d9d18b0bef715cc5a38753f83
DEVAVISWA G <devaviswa15@gmail.com>
2023-11-02 17:10:04 +0530

    basic redux inside main.jsx only

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createStore } from 'redux'
 
+//define a initial state
+let initialState = {
+    count: 0
+}
+//define a reducer
+const countReducer = (state = initialState.count, action) => {
+    switch (action.type) {
+        case 'INCR':
+            return state + 1
+        case 'DECR':
+            return state - 1
+        default:
+            return state
+    }
+}
+
+//create store
+const store = createStore(countReducer)
+console.log(store.getState())  //console to view the current state
+
+//dispatch
+store.dispatch({
+    type: 'INCR'
+})
+console.log(store.getState()) // console to view the changed state
+
+store.dispatch({
+    type: 'INCR'
+})
+console.log(store.getState())
+
+store.dispatch({
+    type: 'INCR'
+})
+console.log(store.getState())
+
+store.dispatch({
+    type: 'DECR'
+})
+console.log(store.getState())
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
