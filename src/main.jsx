17273959064c22899d87a1f283ad25bd14eaec8b
DEVAVISWA G <devaviswa15@gmail.com>
2023-11-02 17:23:55 +0530

    using subscribe() method insted for using multiple console

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,9 @@ const countReducer = (state = initialState.count, action) => {
             return state - 1
         default:
             return state
+        case 'RESET':
+            return 0
+    
     }
 }
 
@@ -23,26 +26,40 @@ const countReducer = (state = initialState.count, action) => {
 const store = createStore(countReducer)
 console.log(store.getState())  //console to view the current state
 
+
+store.subscribe(()=> {
+    console.log(store.getState())
+})
+
 //dispatch
 store.dispatch({
     type: 'INCR'
 })
-console.log(store.getState()) // console to view the changed state
+// console.log(store.getState()) 
 
 store.dispatch({
     type: 'INCR'
 })
-console.log(store.getState())
+// console.log(store.getState()) 
 
 store.dispatch({
     type: 'INCR'
 })
-console.log(store.getState())
+// console.log(store.getState()) 
 
 store.dispatch({
     type: 'DECR'
 })
-console.log(store.getState())
+// console.log(store.getState()) 
 
+store.dispatch({
+    type: 'RESET' 
+})
+// console.log(store.getState()) 
+
+store.subscribe(()=> {
+    console.log(store.getState())
+})
+// console.log(store.getState()) 
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
