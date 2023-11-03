66c8ad234e5fc794d5a34c2a744cc1f573d0c26e
DEVAVISWA G <devaviswa15@gmail.com>
2023-11-03 14:17:40 +0530

    notes with REDUX

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { createStore } from 'redux'
+
+const inputReducer = (state =[] , action) => {
+  if(action.type=='NewNote'){
+    state.push(action.payload)
+    return state
+  }
+  return state
+}
+const inputStore = createStore(inputReducer)
+
+inputStore.dispatch ({
+  type: 'NewNote',
+  payload: 
+    {
+      id: 1,
+      content: 'abcd',
+      importance: false
+    }   
+})
+inputStore.dispatch ({
+  type: 'NewNote',
+  payload: 
+    {
+      id: 2,
+      content: 'efgh',
+      importance: true
+    }   
+})
 
 function App() {
-  const dispatch = useDispatch()
-  const counter = useSelector((state)=> state)
+  console.log(inputStore.getState())
   return (
     <div>
-      Count :- {counter}
-      <br />
-      <button onClick={()=> dispatch({type: 'INCR'})}> inc </button>
-      
-      <button onClick={()=> dispatch({type: 'DECR'})}> dec </button>
-      
-      <button onClick={()=> dispatch({type: 'RESET'})}>reset </button>
+      {
+        inputStore.getState().map(note =>
+          <li key={note.id}>{note.content}</li> )
+      }
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 
-//define a initial state
-let initialState = {
-    count: 0
-}
-//define a reducer
-const countReducer = (state = initialState.count, action) => {
-    switch (action.type) {
-        case 'INCR':
-            return state + 1
-        case 'DECR':
-            return state - 1
-        default:
-            return state
-        case 'RESET':
-            return 0
-    
-    }
-}
- 
-const store = createStore(countReducer)
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-        <App />
-    </Provider>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
